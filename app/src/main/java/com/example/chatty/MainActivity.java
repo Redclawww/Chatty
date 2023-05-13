be23c32d35f3a611e67af34d8a7f530baa76314f
Raghav Sharma <redclaww02@gmail.com>
2023-05-14 03:59:14 +0530

    Created References

--- app/src/main/java/com/example/chatty/MainActivity.java
+++ app/src/main/java/com/example/chatty/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.chatty;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    RecyclerView recyclerView;
+    TextView welcomeTextView;
+    EditText messageEditText;
+    ImageButton sendButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView=findViewById(R.id.recycler_view);
+        welcomeTextView=findViewById(R.id.welcome_text);
+        messageEditText= findViewById(R.id.message_edit_text);
+        sendButton = findViewById(R.id.send_btn);
+
+        sendButton.setOnClickListener((v)->{
+            String question= messageEditText.getText().toString().trim();
+            Toast.makeText(this,question,Toast.LENGTH_LONG).show();
+        } );
+
     }
 }
